be71c2d3872758a5d578aa35b11f0e17c4045061
kobong118 <kobong118@gmail.com>
2023-04-24 09:51:39 +0700

    cari text arabic

--- stemplet bloger/mode-book.js
+++ stemplet bloger/mode-book.js
@@ -22,4 +22,25 @@ ulListMenu[0].hidden = false
     ulListMenu[0].hidden = true}
 mbmenu[0].classList.toggle('hidden')
 };
-ulListMenu[0].onclick = ()=>{mbmenu[0].classList.toggle('hidden'); ulListMenu[0].hidden = true}
+ulListMenu[0].onclick = ()=>{mbmenu[0].classList.toggle('hidden'); ulListMenu[0].hidden = true};
+
+// cari text arabic
+const arb = /[\u0600-\u06ff]/
+const textArb = document.getElementsByTagName("h1");
+const textArb2 = document.getElementsByTagName("span");
+if (textArb.length !=0) {
+    for (let i = 0; i < textArb.length; i++) {
+        if (arb.test(textArb[i].innerHTML)) {
+            textArb[i].classList.add("arb");
+        }
+        
+    }
+}
+if (textArb2.length !=0) {
+    for (let i = 0; i < textArb2.length; i++) {
+        if (arb.test(textArb2[i].innerHTML)) {
+            textArb2[i].classList.add("arb");
+        }
+        
+    }
+}
