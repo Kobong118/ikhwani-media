581a39e4c4073052beaf5f744edca7c0febc9c5d
kobong118 <kobong118@gmail.com>
2023-06-23 00:48:18 +0700

    lk

--- stemplet bloger/asli/main.js
+++ stemplet bloger/asli/main.js
@@ -1,10 +1,1 @@
 // tombol tampil atas menu
-const tAppAtas = document.querySelector('#TappAtas');
-tAppAtas.onclick=()=>{
-    document.querySelector('#MenuAtas').classList.toggle('show');
-    document.querySelector('.atas-menu-item').classList.toggle('show2');
-    setTimeout(()=>{
-        tAppAtas.classList.toggle('nyala')
-    },300)
-    
-};
