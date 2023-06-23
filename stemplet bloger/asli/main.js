421d661d95019bf925f55e3901a715c978bf085d
kobong118 <kobong118@gmail.com>
2023-06-23 22:22:08 +0700

    q

--- stemplet bloger/asli/main.js
+++ stemplet bloger/asli/main.js
@@ -1,1 +1,10 @@
 // tombol tampil atas menu
+const btnCari = document.querySelector(".btn-cari");
+const cari = document.querySelector(".cari")
+btnCari.addEventListener("click",()=>{
+    cari.classList.toggle("tampil-cari");
+});
+
+
+
+
